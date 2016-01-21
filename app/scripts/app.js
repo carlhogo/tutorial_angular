df66786beb4dea9015c8940be3f91a2a4aa5d1b3
Carlos Hernán Hoyos Gómez <carlos.gomez88@gmail.com>
2016-01-21 18:53:41 -0500

    Creación del proyecto

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tutorialAngularApp
+ * @description
+ * # tutorialAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tutorialAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tutorialAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tutorialAngularApp
+ */
+angular.module('tutorialAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

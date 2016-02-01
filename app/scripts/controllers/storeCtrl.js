78252eb19bb63e635b97a2fdcc85178eb26cd524
Carlos Hernán Hoyos Gómez <carlos.gomez88@gmail.com>
2016-02-01 18:12:29 -0500

    reto 3 codeschool

--- app/scripts/controllers/storeCtrl.js
+++ app/scripts/controllers/storeCtrl.js
@@ -38,6 +38,7 @@ var app = angular.module('exampleController', [])
   app.controller('ReviewController', function(){
      this.review = {};
      this.addReview = function(product){
+      this.review = {createdOn : Date.now()};
        product.reviews.push(this.review);
        this.review = {};
      };
